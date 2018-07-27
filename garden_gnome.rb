23621e046a94cfb7f7b8e1d365bd7e0f81fc912b
ziesop20 <sincerelysophie@hotmail.com>
2018-07-27 18:51:28 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,2 +1,14 @@
 # Code your instances here
-class GardenGnome
+class GardenGnome 
+  attr_reader :title 
+  attr_accessor :author, :page_count, :genre
+  def initialize(title)
+  @title= title 
+  end 
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end 
+end 
+
+book = Book.new("Some Title")
